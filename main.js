d1eb9facab6cecbd47f41b81ef90f9f99709cbd6
SG <sarah.gould@gmail.com>
2022-12-02 17:40:57 -0800

    open both dev tools windows in sdk mode

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ const { buildMenu } = require('./script/menu')
 
 nw.Window.open('index.html', {}, mainWindow => {
 	if (process.versions['nw-flavor'] === 'sdk') {
-		// chrome.developerPrivate.openDevTools({
-		// 	renderViewId: -1,
-		// 	renderProcessId: -1,
-		// 	extensionId: chrome.runtime.id
-		// })
+		chrome.developerPrivate.openDevTools({
+			renderViewId: -1,
+			renderProcessId: -1,
+			extensionId: chrome.runtime.id
+		})
 		mainWindow.showDevTools()
 	}
 	mainWindow.menu = buildMenu()
